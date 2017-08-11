6881c8db5f37cf083efc943af0f2daab4f26816b
ksipp01 <ksipp911@gmail.com>
2017-08-11 12:30:59 -0500

    remove auto close email at session end timout set to 60

--- WebApplication1/Success.aspx
+++ WebApplication1/Success.aspx
@@ -2,7 +2,7 @@
 
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
     <div>
-        <asp:Label ID="Label1" runat="server" Text="Thank you! <br /> <br /> Your ETA has been submitted" Font-Names="Arial"></asp:Label>  
+        <asp:Label ID="Label1" runat="server" Text="Thank you! <br /> <br /> Your ETA has been submitted  <br /> Please close this page" Font-Names="Arial"></asp:Label>  
     </div>
 
 
--- WebApplication1/Success.aspx.cs
+++ WebApplication1/Success.aspx.cs
@@ -11,28 +11,14 @@ namespace WebApplication1
 {
     public partial class Success : System.Web.UI.Page
     {
-        //bool autoclose = false;
-        //System.Timers.Timer timer1 = new System.Timers.Timer(5000);
-     
+  
         protected void Page_Load(object sender, EventArgs e)
         {
-            //if (!autoclose)
-            //{
-            //    timer1.Start();
-            //    timer1.Elapsed += OnTimedEvent;
-            //}
-            //else
-                this.ClientScript.RegisterStartupScript(typeof(Page), "closePage", "window.open('close.html', '_self', null);", true);
+           // this will close some browsers doesn't work for safari
+          //      this.ClientScript.RegisterStartupScript(typeof(Page), "closePage", "window.open('close.html', '_self', null);", true);
 
         }
-        //private  void OnTimedEvent(Object source, ElapsedEventArgs e)
-        //{
-        //    timer1.Stop();
-        //    //  ClientScript.RegisterStartupScript(typeof(Page), "closePage", "window.open('close.html', '_self', null);", true);
-        //    autoclose = true;
-            
-        //    //   Page.ClientScript.RegisterClientScriptBlock(typeof(Page), "ClosePage", "window.close();", true);
-        //}
+     
 
       
     }
